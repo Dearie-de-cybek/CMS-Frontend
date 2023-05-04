0fd7000bf7da1b32533e659e1e04fb91bdb257a3
Eburu Dearie <eburujacintha@gmail.com>
2023-05-04 13:51:21 +0100

    Updated navigation

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 'use strict';
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('nav').classList.toggle("open")
+})
+
+document.querySelector('.close-btn').addEventListener('click', () =>{
+    document.querySelector('nav').classList.toggle("open")
+})
