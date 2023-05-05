cf7147a4bf0f510b2ca2645e190cb0597a6898ed
Eburu Dearie <eburujacintha@gmail.com>
2023-05-05 11:13:54 +0100

    About done

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ document.querySelector('.menu-btn').addEventListener('click', () =>{
 document.querySelector('.close-btn').addEventListener('click', () =>{
     document.querySelector('nav').classList.toggle("open")
 })
+
+
+
+var mySwiper = new Swiper ('.swiper-container', {
+    speed: 400,
+    spaceBetween: 100,
+    initialSlide: 0,
+    //truewrapper adoptsheight of active slide
+    autoHeight: false,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // delay between transitions in ms
+    autoplay: 5000,
+    autoplayStopOnLast: false,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+});
